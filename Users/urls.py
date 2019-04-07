0d032a6835c88efb065b7e7cff1d859f747584ba
vishesh <you@example.com>
2019-04-08 00:05:01 +0530

    urls

--- Users/urls.py
+++ Users/urls.py
@@ -15,7 +15,8 @@ urlpatterns = [
     url(r'^auth/registration/complete/$', views.complete_view, name='account_confirm_complete'),
     url(r'^auth/password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.null_view, name='password_reset_confirm'),
     # Default urls
-    url(r'auth/', include('rest_auth.urls')),
+    url(r'^auth/', include('rest_auth.urls')),
+    url(r'^', include('django.contrib.auth.urls')),
     url(r'^auth/registration/', include('rest_auth.registration.urls')),
     #url(r'^check/(?P<consultant_id>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/$', views.check_user),
     #url(r'^profile/',views.UserProfile),
